d038026e70e84cd19121739b6f88355fbd08d901
joravlo <joravlo@gmail.com>
2017-11-02 12:26:39 +0100

    Add slider in index.html

--- js/indexJs.js
+++ js/indexJs.js
@@ -1,10 +1,12 @@
 var imageIndex = 1;
 showDivs(imageIndex);
 
+//Funcion que pasar un index a showDivs
 function nextDiv(index) {
   showDivs(imageIndex += index);
 }
 
+//Funcion que mostrar la imagen segun el indice que se le pase
 function showDivs(index) {
   var element = document.getElementsByClassName("imageSlide");
   if(index > element.length){
@@ -17,4 +19,25 @@ function showDivs(index) {
     element[i].style.display = "none";
 }
   element[imageIndex - 1].style.display = "block";
+}
+
+//Funciones que le daran opacidad a las flechas
+function setOpacityLeft() {
+  var arrowLeft = document.getElementById("arrowLeft");
+  arrowLeft.style.opacity = "0.5";
+}
+
+function removeOpacityLeft() {
+  var arrowLeft = document.getElementById("arrowLeft");
+  arrowLeft.style.opacity = "1";
+}
+
+function setOpacityRight() {
+  var arrowRight = document.getElementById("arrowRight");
+  arrowRight.style.opacity = "0.5";
+}
+
+function removeOpacityRight() {
+  var arrowRight = document.getElementById("arrowRight");
+  arrowRight.style.opacity = "1";
 }
