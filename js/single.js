0a418680c4152c44d9cb3c2c697f5ff276eed553
Arcs96 <arcdj.96@gmail.com>
2017-11-15 12:51:03 +0100

    Single v0.3 final

--- js/single.js
+++ js/single.js
@@ -57,28 +57,33 @@ function muestra(){
 
   /* objeto JSON */
   var advent = '{"sports":[' +
-  '{"id":"1","name":"Escalada","precio":"33€","descripcion":"El esfuerzo y las ganas te llevarán a lo más alto","url1":"images/escalada1.jpg","url2":"images/escalada2.jpg"},' +
-  '{"id":"2","name":"MTB","precio":"36€","descripcion":"Todo lo que sube, tiene que bajar","url1":"images/mtb1.jpg","url2":"images/mtb2.jpg"},' +
-  '{"id":"3","name":"Airsoft","precio":"45€","descripcion":"Vive experiencias que nunca olvidarás","url":"1"},' +
+  '{"id":"1","nom":"Escalada","precio":"33€","descripcion":"El esfuerzo y las ganas te llevarán a lo más alto","url1":"images/escalada1.jpg","url2":"images/escalada2.jpg"},' +
+  '{"id":"2","nom":"MTB","precio":"36€","descripcion":"Todo lo que sube, tiene que bajar","url1":"images/mtb1.jpg","url2":"images/mtb2.jpg"},' +
+  '{"id":"3","nom":"Airsoft","precio":"45€","descripcion":"Vive experiencias que nunca olvidarás","url":"1"},' +
 
-  '{"id":"4","name":"Rafting","precio":"49,99€","descripcion":"No existen los atajos para los lugares que valen la pena","url":"1"},' +
-  '{"id":"5","name":"Buceo","precio":"52€","descripcion":"Atraviesa la superficie y descubre un nuevo mundo","url":"1"},' +
-  '{"id":"6","name":"Kayak","precio":"42€","descripcion":"A veces no se tiene el viento a favor, pero hay que seguir remando","url":"1"},' +
+  '{"id":"4","nom":"Rafting","precio":"49,99€","descripcion":"No existen los atajos para los lugares que valen la pena","url1":"images/rafting1.jpg","url2":"images/rafting2.jpg"},' +
+  '{"id":"5","nom":"Buceo","precio":"52€","descripcion":"Atraviesa la superficie y descubre un nuevo mundo","url1":"images/buceo1.jpg","url2":"images/buceo2.jpg"},' +
+  '{"id":"6","nom":"Kayak","precio":"42€","descripcion":"A veces no se tiene el viento a favor, pero hay que seguir remando","url1":"images/kayak1.jpg","url2":"images/kayak2.jpg"},' +
 
-  '{"id":"7","name":"Puenting","precio":"44€","descripcion":"Vence tus miedos","url":"1"},' +
-  '{"id":"8","name":"Raúl","precio":"75,95€","descripcion":"Sentirse libre aunque solo sea por un momento","url":"1"}]}';
+  '{"id":"7","nom":"Puenting","precio":"44€","descripcion":"Vence tus miedos","url1":"images/puenting1.jpg","url2":"images/puenting2.jpg"},' +
+  '{"id":"8","nom":"Paracaidismo","precio":"75,95€","descripcion":"Sentirse libre aunque solo sea por un momento","url1":"images/paracaidismo1.jpg","url2":"images/paracaidismo2.jpg"}]}';
 
   /* declaramos el objeto JSON */
   obj = JSON.parse(advent);
 
   /* recorremos el objeto JSON */
   for(var i in obj.sports) {
+    /* Recorremos el JSON y con id recogida en localStorage, comprueba las id
+       de los elementos del JSON par*/
     if (obj.sports[i].id==opcion) {
+      document.getElementById("titulo").innerHTML = obj.sports[i].nom;
+      document.getElementById("img1").src = obj.sports[i].url1;
+      document.getElementById("img2").src = obj.sports[i].url2;
       document.getElementById("descr").innerHTML = obj.sports[i].descripcion;
       document.getElementById("precio").innerHTML = obj.sports[i].precio;
     }
   }
-
+  localStorage.clear();
 }
 
 /*function myOverFunction() {
