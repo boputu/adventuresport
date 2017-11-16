90d3c856f35c18b7dfd7a665c0e5f6cba05cc323
joravlo <joravlo@gmail.com>
2017-11-16 13:45:21 +0100

    Add shoppingCart

--- js/shoppingCart.js
+++ js/shoppingCart.js
@@ -1,11 +1,16 @@
 
 function addShoppingCart() {
+  var select = document.getElementById("selectQuantity");
+  var selectQuantity = select.options[select.selectedIndex].value;
+  var productName = document.getElementById("titulo").textContent;
+  var priceProduct = document.getElementById("precio").textContent;
+  var imgProduct = document.getElementById("img1").src;
   //Creamos el objeto que se añadira al carrito. Contiene dentro una funcion que para añadirlo al localStorage
   var objectCart = {
-    name: "PEPE",
-    quantity: 2,
-    price: 12.00,
-    image: "",
+    name: productName,
+    quantity: selectQuantity,
+    price: priceProduct,
+    image: imgProduct,
     saveObject: function () {
       var shoppingCart = new Array();
         // Parse the serialized data back into an aray of objects
@@ -19,10 +24,14 @@ function addShoppingCart() {
         localStorage.setItem('shoppingCart', JSON.stringify(shoppingCart));
     }
   };
+
+  objectCart.saveObject();
 }
 
+//Funcion que creara los objetos del carrito
 function showShoppingCart() {
-  for (var i = 0; i < 3; i++) {
+  var cart = JSON.parse(localStorage.getItem('shoppingCart'));
+  for (var i = 0; i < cart.length; i++) {
 
     var ul = document.createElement("ul");
     ul.className = "sub-icon1 list";
@@ -33,18 +42,20 @@ function showShoppingCart() {
     var liImg = document.createElement("li");
     liImg.className = "list_img";
     var img = document.createElement("img");
-    img.src = "images/1.jpg";
+    img.style.height = '100px';
+    img.style.width = '100px';
+    img.src = cart[i].image;
     liImg.appendChild(img);
     ul.appendChild(liImg);
 
     var liDesc = document.createElement("li");
     liDesc.className = "list_desc";
     var h4 = document.createElement("h4");
-    h4.innerHTML = "velit esse molestie";
+    h4.innerHTML = cart[i].name;
     liDesc.appendChild(h4);
     var span = document.createElement("span");
     span.className = "actual";
-    span.innerHTML = "1 x $12.00";
+    span.innerHTML = cart[i].quantity + " x " + cart[i].price;
     liDesc.appendChild(span);
     ul.appendChild(liDesc);
 
